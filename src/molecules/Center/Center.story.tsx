3c2a031bb0d349a31c2a9f9cc3937dabc5d114d0
shiropt <shiro19880610@yahoo.co.jp>
2023-11-04 18:54:27 +0900

    feat: Centerコンポーネントのstorybookを作成

---
+++ src/molecules/Center/Center.story.tsx
@@ -0,0 +1,23 @@
+import { Center as CenterComponent } from '.';
+
+export default {
+  title: 'molecules/Center',
+};
+
+export const Center = () => (
+  <CenterComponent title="タイトル">
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt totam dolorem consequuntur,
+      deleniti necessitatibus numquam ipsum molestias, quos nostrum ipsam eum, non quas quaerat
+      explicabo voluptatibus error! Dicta, maxime perspiciatis? Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Pariatur, officiis saepe! Nihil temporibus sunt mollitia eum?
+      Corporis aliquam doloremque voluptates ipsa quis accusamus et, earum rerum molestias in
+      dolores perspiciatis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat modi
+      facilis expedita voluptatibus amet illo minus repellendus perspiciatis nesciunt,
+      exercitationem, magnam a inventore non tempora minima dolorem voluptate est consequatur! Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Odio ullam corrupti esse aspernatur
+      laudantium iste itaque porro voluptatibus ad, sequi assumenda illum iure fugit sint minima
+      totam laboriosam reprehenderit necessitatibus.
+    </div>
+  </CenterComponent>
+);
