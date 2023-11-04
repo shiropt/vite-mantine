583c7d49dec84b8ada075b33a28b3af2fc318a56
shiropt <shiro19880610@yahoo.co.jp>
2023-11-04 09:07:44 +0900

    feat: Inputコンポーネントを作成

---
+++ src/components/atoms/Input/index.tsx
@@ -0,0 +1,13 @@
+import { Input as MantineInput, InputWrapperProps, InputProps } from '@mantine/core';
+import { FC } from 'react';
+
+type Props = InputProps & InputWrapperProps;
+
+export const Input: FC<Props> = (props) => (
+  <MantineInput.Wrapper
+    styles={{ label: { marginLeft: '4px' }, error: { marginLeft: '4px' } }}
+    {...props}
+  >
+    <MantineInput {...props} />
+  </MantineInput.Wrapper>
+);
