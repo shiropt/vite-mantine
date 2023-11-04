c490ab7552eb963a5e272964462b6b491ca7f14f
shiropt <shiro19880610@yahoo.co.jp>
2023-11-05 08:40:26 +0900

    fix: Centerコンポーネントのレイアウトを微修正

--- src/components/molecules/Center/index.tsx
+++ src/components/molecules/Center/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Title } from '@mantine/core';
+import { Box, Container, Flex, Title } from '@mantine/core';
 import { FC, ReactNode } from 'react';
 
 type Props = {
@@ -7,16 +7,18 @@ type Props = {
 };
 
 export const Center: FC<Props> = ({ title, children }) => (
-  <Flex justify="center" bg="white" w="60%" mih={600}>
-    <Box>
-      <Flex justify="center">
-        <Title mt="lg" p="lg" order={2}>
-          {title}
-        </Title>
-      </Flex>
-      <Flex p="lg" justify="center">
-        {children}
-      </Flex>
-    </Box>
-  </Flex>
+  <Container bg="white" size="xs" p="md">
+    <Flex justify="center">
+      <Box>
+        <Flex justify="center">
+          <Title p="md" order={2}>
+            {title}
+          </Title>
+        </Flex>
+        <Flex p="lg" justify="center">
+          {children}
+        </Flex>
+      </Box>
+    </Flex>
+  </Container>
 );
