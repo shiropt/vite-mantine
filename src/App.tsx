7204a90bbad50e643207471ad91f65e90409df09
shiropt <shiro19880610@yahoo.co.jp>
2023-11-05 22:27:13 +0900

    feat: Contextモーダルを実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
+import { ModalsProvider } from '@mantine/modals';
 import { Router } from './libs/Router';
 import { theme } from './theme';
 import './style/global.css';
+import { OKButtonModal } from './components/molecules/Modals';
 
 export default function App() {
   return (
     <MantineProvider theme={theme}>
-      <Router />
+      <ModalsProvider modals={{ OKButtonModal }}>
+        <Router />
+      </ModalsProvider>
     </MantineProvider>
   );
 }
