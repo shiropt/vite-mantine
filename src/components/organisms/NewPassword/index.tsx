6d12423acd8045b92e45d1eab3d207d74f58ac87
shiropt <shiro19880610@yahoo.co.jp>
2023-11-05 22:57:53 +0900

    feat: 「新しいパスワードを登録」からの画面遷移を実装

--- src/components/organisms/NewPassword/index.tsx
+++ src/components/organisms/NewPassword/index.tsx
@@ -1,10 +1,13 @@
 import { FC } from 'react';
 import { ContainerProps, Flex } from '@mantine/core';
 import { useForm, zodResolver } from '@mantine/form';
+import { useNavigate } from 'react-router-dom';
 import { Center } from '../../molecules/Center';
 import { Input } from '../../atoms/Input';
 import { Button } from '../../atoms/Button';
 import { schema } from '../../../libs/zod/schema';
+import { openOKButtonModal } from '../../molecules/Modals';
+import { path } from '../../../libs/Router/path';
 
 type Props = ContainerProps;
 
@@ -15,11 +18,16 @@ export const NewPassword: FC<Props> = (props) => {
     },
     validate: zodResolver(schema.newPassword),
   });
+  const navigate = useNavigate();
+
+  const openModal = () => {
+    openOKButtonModal('新しいパスワードを登録しました。', () => navigate(path.root));
+  };
 
   return (
     <Center {...props} title="新しいパスワードを設定">
       <Flex direction="column">
-        <form onSubmit={onSubmit((values) => console.log(values))}>
+        <form onSubmit={onSubmit(openModal)}>
           <Flex direction="column" mb="md">
             <Input
               mb="xs"
