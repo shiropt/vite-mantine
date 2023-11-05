529a61aa842ee053af27563202af16ccfeb3fb50
shiropt <shiro19880610@yahoo.co.jp>
2023-11-05 22:57:21 +0900

    feat: 「パスワードをお忘れの方」からの画面遷移を実装

--- src/components/organisms/PasswordForgot/index.tsx
+++ src/components/organisms/PasswordForgot/index.tsx
@@ -1,25 +1,43 @@
 import { FC } from 'react';
 import { ContainerProps, Flex } from '@mantine/core';
 import { useForm, zodResolver } from '@mantine/form';
+import { useNavigate } from 'react-router-dom';
 import { Center } from '../../molecules/Center';
 import { Input } from '../../atoms/Input';
 import { Button } from '../../atoms/Button';
 import { schema } from '../../../libs/zod/schema';
+import { openOKButtonModal } from '../../molecules/Modals';
+import { path } from '../../../libs/Router/path';
 
 type Props = ContainerProps;
 
 export const PasswordForgot: FC<Props> = (props) => {
-  const { onSubmit, getInputProps } = useForm({
+  const {
+    onSubmit,
+    getInputProps,
+    values: { email },
+  } = useForm({
     initialValues: {
       email: '',
     },
     validate: zodResolver(schema.passwordForgot),
   });
+  const navigate = useNavigate();
+
+  const openModal = () => {
+    openOKButtonModal(
+      <p>
+        パスワード再設定メールを送信しました。 <br />
+        内容をご確認の上、新しいパスワードを設定してください。
+      </p>,
+      () => navigate(path.newPassword, { state: { email } })
+    );
+  };
 
   return (
     <Center {...props} title="パスワードをお忘れの方">
       <Flex direction="column">
-        <form onSubmit={onSubmit((values) => console.log(values))}>
+        <form onSubmit={onSubmit(openModal)}>
           <Flex direction="column" mb="md">
             <Input
               mb="xs"
