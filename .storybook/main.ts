e62b2d68c096cb6e16e6b1b2d28bb440802f3cec
shiropt <shiro19880610@yahoo.co.jp>
2023-11-08 21:12:31 +0900

    feat: add storybook-addon-react-router-v6

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,12 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.story.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-styling', 'storybook-dark-mode'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-styling',
+    'storybook-dark-mode',
+    'storybook-addon-react-router-v6',
+  ],
   framework: {
     name: '@storybook/react-vite',
     options: {},
