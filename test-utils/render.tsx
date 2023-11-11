728095a2a0d4be2012b06e1fe228d4170677353f
shiropt <shiro19880610@yahoo.co.jp>
2023-11-11 21:13:54 +0900

    test: テスト用のwrapperにReact-RouterとuseEventを追加

--- test-utils/render.tsx
+++ test-utils/render.tsx
@@ -1,11 +1,20 @@
 import { render as testingLibraryRender } from '@testing-library/react';
 import { MantineProvider } from '@mantine/core';
+import { BrowserRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 import { theme } from '../src/theme';
 
-export function render(ui: React.ReactNode) {
-  return testingLibraryRender(<>{ui}</>, {
-    wrapper: ({ children }: { children: React.ReactNode }) => (
-      <MantineProvider theme={theme}>{children}</MantineProvider>
-    ),
-  });
-}
+export const render = (ui: React.ReactNode, { route = '/' } = {}) => {
+  window.history.pushState({}, 'Test page', route);
+
+  return {
+    ...testingLibraryRender(<>{ui}</>, {
+      wrapper: ({ children }: { children: React.ReactNode }) => (
+        <MantineProvider theme={theme}>
+          <BrowserRouter>{children}</BrowserRouter>
+        </MantineProvider>
+      ),
+    }),
+    user: userEvent.setup(),
+  };
+};
