d988f0b9a0eaf616c7817ab206821f7335c26911
shiropt <shiro19880610@yahoo.co.jp>
2023-11-19 16:21:17 +0900

    feat: ログインボタン(仮)を作成

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,11 +1,22 @@
+import { useAuth0 } from '@auth0/auth0-react';
+import { Box } from '@mantine/core';
 import { Welcome } from '../components/Welcome/Welcome';
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import { Button } from '../components/atoms/Button';
 
 export function HomePage() {
+  const { logout } = useAuth0();
+
   return (
-    <>
+    <Box ta="center">
       <Welcome />
-      <ColorSchemeToggle />
-    </>
+      <Button
+        mt="xl"
+        onClick={() => {
+          logout();
+        }}
+      >
+        ログアウト
+      </Button>
+    </Box>
   );
 }
