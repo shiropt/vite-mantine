ff0f44febde5209ddeb7364214103bc6b8719067
shiropt <shiro19880610@yahoo.co.jp>
2023-12-17 17:38:08 +0900

    feat: show account in Welcome component

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -1,23 +1,23 @@
-import { Title, Text, Anchor } from '@mantine/core';
+import { Title, Text } from '@mantine/core';
+import { FC } from 'react';
 import classes from './Welcome.module.css';
+import { Account } from '../../modules/account/type';
 
-export function Welcome() {
-  return (
-    <>
-      <Title className={classes.title} ta="center" mt={100}>
-        Welcome to{' '}
-        <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'yellow' }}>
-          Mantine
-        </Text>
-      </Title>
-      <Text c="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
-        This starter Vite project includes a minimal setup, if you want to learn more on Mantine +
-        Vite integration follow{' '}
-        <Anchor href="https://mantine.dev/guides/vite/" size="lg">
-          this guide
-        </Anchor>
-        . To get started edit pages/Home.page.tsx file.
+type Props = {
+  account: Account;
+};
+
+export const Welcome: FC<Props> = ({ account }) => (
+  <>
+    <Title className={classes.title} ta="center" mt={100}>
+      Welcome to{' '}
+      <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'yellow' }}>
+        Mantine
       </Text>
-    </>
-  );
-}
+    </Title>
+    <Text> hello {account.name}</Text>
+    <Text>
+      <img alt="icon" src={account.picture}></img>
+    </Text>
+  </>
+);
--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,14 +1,24 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import { Box } from '@mantine/core';
+import { useEffect } from 'react';
 import { Welcome } from '../components/Welcome/Welcome';
 import { Button } from '../components/atoms/Button';
+import { useDispatch, useSelector } from '../hooks/redux';
+import { actions } from '../modules/account';
 
 export function HomePage() {
-  const { logout } = useAuth0();
+  const { logout, user } = useAuth0();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (user) {
+      dispatch(actions.setAccount(user));
+    }
+  }, []);
+  const account = useSelector((state) => state.account);
 
   return (
     <Box ta="center">
-      <Welcome />
+      <Welcome account={account} />
       <Button
         mt="xl"
         onClick={() => {
