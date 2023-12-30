8a6e2b9cfd7a703eaf1fb0b24c669caf79d0c2ca
shiropt <shiro19880610@yahoo.co.jp>
2023-12-31 08:04:01 +0900

    fix Input props

--- src/components/atoms/Input/index.tsx
+++ src/components/atoms/Input/index.tsx
@@ -1,9 +1,9 @@
 import { Input as MantineInput, InputWrapperProps, InputProps } from '@mantine/core';
-import { FC } from 'react';
+import { ComponentProps, FC } from 'react';
 
-type Props = InputProps & InputWrapperProps;
+type Props = InputProps & InputWrapperProps & ComponentProps<'input'>;
 
-export const Input: FC<Props> = (props) => (
+export const Input: FC<Props> = ({ ref, ...props }) => (
   <MantineInput.Wrapper
     styles={{ label: { marginLeft: '4px' }, error: { marginLeft: '4px' } }}
     {...props}
